1e80ecab76668f12223e75ee2ccee739da2c7317
Abubakr Hamid <abubakr.hamid@canadianblackbook.com>
2023-08-14 16:31:21 -0400

    More tests

--- test.ts
+++ test.ts
@@ -3,6 +3,7 @@ import { assertEquals } from "https://deno.land/std@0.198.0/assert/mod.ts";
 import { Country, getCountries } from "./mod.ts";
 
 const countries: Country[] = await getCountries("Canada");
+console.log(countries);
 
 Deno.test("Result length",() => {
     assertEquals(countries.length, 1);
@@ -17,4 +18,27 @@ Deno.test("Country CCA2",  () => {
 })
 Deno.test("Country CCA3",() => {
     assertEquals(countries[0].cca3 , "CAN");
+})
+Deno.test("Country region",() => {
+    assertEquals(countries[0].region , "Americas");
+})
+Deno.test("Country currency",() => {
+    assertEquals(countries[0].currency , "Canadian dollar");
+})
+
+Deno.test("Country currencyCode",() => {
+    assertEquals(countries[0].currencyCode , "CAD");
+})
+
+Deno.test("Country currencySymbol",() => {
+    assertEquals(countries[0].currencySymbol , "$");
+})
+Deno.test("Country callCode",() => {
+    assertEquals(countries[0].callCode , "+1");
+})
+Deno.test("Country latLng",() => {
+    assertEquals(countries[0].latLng , [ 60, -95 ]);
+})
+Deno.test("Country flag",() => {
+    assertEquals(countries[0].flag ,"https://flagcdn.com/w320/ca.png");
 })
